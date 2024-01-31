29ff2730bdf9bfc8983222e9cc3a30a8b700f031
Falcon Pioupiou <falcon-pioupiou@users.noreply.github.com>
2024-01-31 11:43:00 +0100

    update motd

--- bootstrap.sh
+++ bootstrap.sh
@@ -118,8 +118,8 @@ sudo chmod -x /etc/update-motd.d/10-help-text
 sudo chmod -x /etc/update-motd.d/50-landscape-sysinfo
 sudo apt-get install -y figlet
 
-sudo echo "#!/bin/bash" | sudo tee /etc/update-motd.d/11-cs-logo
-sudo echo "figlet -f slant CrowdStrike" | sudo tee /etc/update-motd.d/11-cs-logo
+sudo echo '#!/bin/bash' | sudo tee /etc/update-motd.d/11-cs-logo
+sudo echo 'figlet "CrowdStrike Lab"' | sudo tee /etc/update-motd.d/11-cs-logo
 sudo chmod +x /etc/update-motd.d/11-cs-logo
 
 
